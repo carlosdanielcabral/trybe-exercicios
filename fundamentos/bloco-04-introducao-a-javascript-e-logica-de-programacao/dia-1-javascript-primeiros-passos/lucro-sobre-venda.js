4a8085e5c1bfaa50b7a8ccdd286cbfd6b3d8f587
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2021-10-08 19:21:05 -0300

    Exercicio 10

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/lucro-sobre-venda.js
@@ -0,0 +1,10 @@
+const custoDoProduto = 20;
+const valorDeVenda = 30;
+
+let impostoSobreOCusto = custoDoProduto * 0.20;
+
+let valorCustoTotal = custoDoProduto + impostoSobreOCusto;
+
+let lucro = valorDeVenda - valorCustoTotal;
+
+let lucroMilProdutos = lucro * 1000;
