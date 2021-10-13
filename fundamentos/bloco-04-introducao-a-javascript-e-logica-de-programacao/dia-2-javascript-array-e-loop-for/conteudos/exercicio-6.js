5d47425472e8b4acc3e08566a6095508be729556
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2021-10-13 18:59:25 -0300

    Exercicio 6

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/conteudos/exercicio-6.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let numeroDeImpares = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+    let modulo = numbers[i] % 2;
+    if (modulo !== 0) {
+        numeroDeImpares += 1;
+    }
+}
+
+console.log(numeroDeImpares);
