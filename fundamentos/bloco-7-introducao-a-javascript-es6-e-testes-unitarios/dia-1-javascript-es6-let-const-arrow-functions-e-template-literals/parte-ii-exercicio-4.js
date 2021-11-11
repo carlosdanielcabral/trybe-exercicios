e34cf1db44caf31e23601379bd472991882a1f68
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2021-11-10 21:57:50 -0300

    Parte II - segunda parte do exercicio 4

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-ii-exercicio-4.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-ii-exercicio-4.js
@@ -1,9 +1,20 @@
 const skills = ['html', 'css', 'javascript', 'git', 'github'];
-const stringQualquer = 'Olá, x!';
+const stringQualquer = 'Olá, meu nome é x';
 
 const novaString = string => {
     let stringModificada = stringQualquer.includes('x') ? stringQualquer.replace('x', string) : stringQualquer;
     return stringModificada;
 }
 
-console.log(novaString('Carlos'));
+const retornarNomeEHabilidades = funcao => {
+    skills.sort();
+    let skillsString = skills.join(';\n');
+
+    let novaString = `${funcao} e minhas principais habilidades são: 
+${skillsString}
+#goTrybe`;
+
+    return novaString;
+}
+
+console.log(retornarNomeEHabilidades(novaString('Carlos')));
