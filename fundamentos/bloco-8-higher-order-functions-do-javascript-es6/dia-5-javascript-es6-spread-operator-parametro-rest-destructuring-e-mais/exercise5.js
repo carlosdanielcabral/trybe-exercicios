f38e22d44229ca68ff4c6f20f0129d482674da47
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2021-11-23 18:57:27 -0300

    Exercicio 5

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise5.js
@@ -0,0 +1,6 @@
+// Função que recebe um array de 3 elementos e retorno um novo array com o 1 e 3 elementos trocados, sendo que deve ser gasta apenas uma linha.
+
+const { myList } = require('./data');
+
+// el = elemento
+const swap = ([ el1, el2, el3 ]) => [ el3, el2, el1 ];
